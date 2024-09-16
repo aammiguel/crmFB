171916bc5b0cb56a80ecd9416d4957152707e778
Miguel Alvarado <angel-mma@hotmail.com>
2024-09-15 22:39:25 -0700

    First Commit

---
+++ src/main/java/com/crmFB/crmFB/CrmFbApplication.java
@@ -0,0 +1,13 @@
+package com.crmFB.crmFB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmFbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmFbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crmFB/crmFB/CrmFbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crmFB.crmFB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmFbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
